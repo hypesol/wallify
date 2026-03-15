4272afa4626a2dc02d893c61dfd22ee99527bec7
Ejaz Ahmad <hypesol@gmail.com>
2026-03-15 22:02:02 +0500

    wallpapers

--- generate-wallpapers.js
+++ generate-wallpapers.js
@@ -1,28 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-const wallpapersDir = "./wallpapers";
+const wallpapersDir = "./wallpapers/full";
 
 let data = {};
 
-fs.readdirSync(wallpapersDir).forEach(type => {
-  const typePath = path.join(wallpapersDir, type);
+fs.readdirSync(wallpapersDir).forEach(category => {
+  const categoryPath = path.join(wallpapersDir, category);
 
-  if (fs.statSync(typePath).isDirectory()) {
-    data[type] = {};
+  if (fs.statSync(categoryPath).isDirectory()) {
+    data[category] = [];
 
-    fs.readdirSync(typePath).forEach(category => {
-      const categoryPath = path.join(typePath, category);
-
-      if (fs.statSync(categoryPath).isDirectory()) {
-        data[type][category] = [];
-
-        fs.readdirSync(categoryPath).forEach(file => {
-          if (file.endsWith(".jpg") || file.endsWith(".webp")) {
-            const imagePath = `${type}/${category}/${file}`;
-            data[type][category].push(imagePath);
-          }
-        });
+    fs.readdirSync(categoryPath).forEach(file => {
+      if (file.endsWith(".jpg") || file.endsWith(".webp")) {
+        const imagePath = `${category}/${file}`;
+        data[category].push(imagePath);
       }
     });
   }
